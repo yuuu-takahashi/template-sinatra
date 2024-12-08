cb3bc5a69ad2bc85ee6ad6efb41fed41897eac63
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2024-12-08 13:08:11 +0000

    sinatra-contribを追加
    ルートエンドポイント作成

--- Gemfile
+++ Gemfile
@@ -6,10 +6,10 @@ end
 
 group :development do
   gem 'ruby-lsp', require: false
+  gem 'sinatra-contrib', require: false
 end
 
 gem 'puma', '~> 6.5'
 gem 'rackup', '~> 2.2'
 gem 'sinatra', '~> 4.1'
-
 gem "mysql2", "~> 0.5.6"
--- index.rb
+++ index.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'sinatra/reloader' if development?
+
 require_relative 'config/database'
 require_relative 'db/schema'
 require_relative 'db/seeds'
+
+get '/' do
+  'Hello, world!'
+end
+
 require_relative 'app/routes/users'
 
 client = database_client
