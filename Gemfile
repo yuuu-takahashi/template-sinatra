a48e64266e21f3e336d5b4c2d03b0e1fd6ea967e
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2024-12-08 08:37:11 +0000

    add: mysql

--- Gemfile
+++ Gemfile
@@ -11,3 +11,5 @@ end
 gem 'puma', '~> 6.5'
 gem 'rackup', '~> 2.2'
 gem 'sinatra', '~> 4.1'
+
+gem "mysql2", "~> 0.5.6"
--- index.rb
+++ index.rb
@@ -1,6 +1,30 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'mysql2'
+
+client = Mysql2::Client.new(
+  host: ENV.fetch('DATABASE_HOST', nil),
+  username: ENV.fetch('DATABASE_USER', nil),
+  password: ENV.fetch('DATABASE_PASSWORD', nil)
+)
+
+database_name = 'template-sinatra_development'
+
+begin
+  result = client.query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '#{database_name}'")
+
+  if result.count.zero?
+    client.query("CREATE DATABASE `#{database_name}`")
+    puts "Database '#{database_name}' created successfully!"
+  else
+    puts "Database '#{database_name}' already exists."
+  end
+rescue Mysql2::Error => e
+  puts "An error occurred: #{e.message}"
+ensure
+  client.close
+end
 
 get '/' do
   'Hello world!'
