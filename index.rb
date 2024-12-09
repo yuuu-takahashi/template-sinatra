6c2d2f9fe3b5624c94954f6f2636df060b4adf75
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2024-12-09 12:57:25 +0000

    Docker環境の設定を更新
    ポート4567公開
    起動コマンド追加

--- index.rb
+++ index.rb
@@ -5,6 +5,9 @@ require 'sinatra/reloader' if development?
 require_relative 'db/setup' if ENV['RUN_SETUP'] == 'true'
 require_relative 'app/controllers/users_controller'
 
+set :bind, '0.0.0.0'
+set :port, 4567
+
 use UsersController
 
 get '/' do
