b8f166491924b5173adb7c0b196fbae560edb711
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2024-12-18 09:46:07 +0000

    add: routes

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_with_alias('@/app/controllers/users_controller')
+
+get '/' do
+  'TOP'
+end
+
+ROUTES = {
+  get: {
+    '/users' => [UsersController, :index],
+    '/users/:id' => [UsersController, :show]
+  }
+}.freeze
+
+ROUTES.each do |http_method, paths|
+  paths.each do |path, (controller, action)|
+    send(http_method, path) do
+      if params.empty?
+        controller.send(action)
+      else
+        controller.send(action, params)
+      end
+    end
+  end
+end
--- index.rb
+++ index.rb
@@ -4,30 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 require_relative 'config/setup'
-require_with_alias('@/app/controllers/users_controller')
+require_with_alias('@/config/routes')
 
 set :bind, '0.0.0.0'
 set :port, 4567
-
-get '/' do
-  'TOP'
-end
-
-ROUTES = {
-  get: {
-    '/users' => [UsersController, :index],
-    '/users/:id' => [UsersController, :show]
-  }
-}.freeze
-
-ROUTES.each do |http_method, paths|
-  paths.each do |path, (controller, action)|
-    send(http_method, path) do
-      if params.empty?
-        controller.send(action)
-      else
-        controller.send(action, params)
-      end
-    end
-  end
-end
