21092533d1dab1a5cc63dd37244ce63c34058ce7
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2024-12-29 16:55:40 +0000

    feat: update Docker Compose and environment configuration; improve file loading in index.rb

--- index.rb
+++ index.rb
@@ -2,13 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 require_relative 'config/environment'
-require_with_alias('@/lib/database_client')
-require_with_alias('@/app/models/user')
-require_with_alias('@/app/controllers/top_controller')
-require_with_alias('@/app/controllers/users_controller')
-require_with_alias('@/config/routes')
 
-Dir[File.join(__dir__, '{app,config,db,spen}/**/*.rb')].each { |file| also_reload file } if development?
+Dir[File.join(__dir__, '{app,config,db,lib}/**/*.rb')].each do |file|
+  require_relative file
+  also_reload file if development?
+end
 
 set :bind, '0.0.0.0'
 set :port, 4567
