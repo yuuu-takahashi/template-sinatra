77f4143c3ad9aed1bda3d0e9182f544ad49c8c7b
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2024-12-31 13:04:29 +0000

    fix

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,3 @@ raise "Error: Env file not found at #{env_file}" unless File.exist?(env_file)
 puts 'Env file found'
 
 Dotenv.overload(env_file)
-
-def require_with_alias(path)
-  require path.sub('@/', "#{APP_ROOT}/")
-end
